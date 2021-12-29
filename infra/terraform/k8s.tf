a45f629932624eece5bfa5029631fececf5c8a19
Yousuf Jawwad <yousuf@maher.pk>
2021-12-30 03:22:35 +0500

    chore(infra): updated infra to prepeare for kubernetes

--- infra/terraform/k8s.tf
+++ infra/terraform/k8s.tf
@@ -1,8 +1,8 @@
-resource "google_artifact_registry_repository" "backstage_backend" {
+resource "google_artifact_registry_repository" "backstage" {
   provider      = google-beta
   project       = var.project
   location      = var.region
-  repository_id = "${local.computed_name}-backend"
+  repository_id = local.computed_name
   format        = "DOCKER"
 
   labels = {
@@ -11,23 +11,51 @@ resource "google_artifact_registry_repository" "backstage_backend" {
   }
 }
 
-resource "google_artifact_registry_repository" "backstage_frontend" {
-  provider      = google-beta
-  project       = var.project
-  location      = var.region
-  repository_id = "${local.computed_name}-frontend"
-  format        = "DOCKER"
+data "google_client_config" "default" {}
 
-  labels = {
-    application = "backstage"
-    environment = "poc"
+provider "kubernetes" {
+  host                   = "https://${module.backstage_gke.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(module.backstage_gke.ca_certificate)
+}
+
+module "backstage_gke" {
+  source                    = "github.com/terraform-google-modules/terraform-google-kubernetes-engine?ref=v17.3.0"
+  project_id                = var.project
+  name                      = local.computed_name
+  region                    = var.region
+  network                   = google_compute_network.backstage.name
+  subnetwork                = google_compute_subnetwork.backstage.name
+  ip_range_pods             = "${local.computed_name}-gke-pods"
+  ip_range_services         = "${local.computed_name}-gke-services"
+  create_service_account    = false
+  service_account           = google_service_account.backstage.email
+  default_max_pods_per_node = 32
+
+  node_pools = [
+    {
+      name               = "${local.computed_name}-gke-node-pool"
+      machine_type       = "e2-medium"
+      min_count          = 1
+      max_count          = 8
+      local_ssd_count    = 0
+      disk_size_gb       = 10
+      disk_type          = "pd-ssd"
+      image_type         = "COS"
+      auto_repair        = true
+      auto_upgrade       = true
+      service_account    = google_service_account.backstage.email
+      preemptible        = false
+      initial_node_count = 1
+      preemptible        = true
+    },
+  ]
+
+  node_pools_labels = {
+    all = {
+      application = "backstage"
+      environment = "poc"
+    }
   }
 }
 
-# module "k8_backstage_cluster" {
-#   source = "../../../../breu/terraform/terraform-gcp-kubernetes"
-
-#   project   = var.project
-#   name      = local.computed_name
-#   namespace = "backstage"
-# }
