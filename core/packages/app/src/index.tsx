5cb4956c91fcf200b78709ce11c9bf0093d527f2
Yousuf Jawwad <yousuf@maher.pk>
2022-01-08 17:39:32 +0500

    chore(infra): firebase hosting configuration for backstage frontend [sc-2427]

--- core/packages/app/src/index.tsx
+++ core/packages/app/src/index.tsx
@@ -3,4 +3,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+/**
+ * Firebase App Initialization
+ * https://firebase.google.com/docs/web/setup#initialize_the_sdk
+ **/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyC3zVI0TnzIg3S3mZu_v2ZW8GnV4DR6ABI',
+  authDomain: 'hd-backstage-poc-28107.firebaseapp.com',
+  projectId: 'hd-backstage-poc-28107',
+  storageBucket: 'hd-backstage-poc-28107.appspot.com',
+  messagingSenderId: '658501036953',
+  appId: '1:658501036953:web:7ff7b31f8420de42c38108',
+  measurementId: 'G-HMY1H500CK',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/**
+ * React App Initialization
+ */
 ReactDOM.render(<App />, document.getElementById('root'));
