8361e1787cdecca7a297039388133e8e185dd859
Yousuf Jawwad <yousuf@maher.pk>
2022-02-24 19:55:30 +0500

    chore(infra): added ip address updated dependencies

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -23,9 +23,24 @@ output "dns_managed_zone_name_servers" {
   value       = google_dns_managed_zone.backstage.name_servers
 }
 
-output "network_peering_address" {
-  description = "Network Peering Address"
+output "network" {
+  description = "VPC Network"
+  value       = local.network__name
+}
+
+output "network_peering_address_name" {
+  description = "Network Peering Address Name"
+  value       = google_compute_global_address.backstage_peering_range_address.name
+}
+
+output "network_peering_address_ip" {
+  description = "Network Peering Address IP"
   value       = google_compute_global_address.backstage_peering_range_address.address
+}
+
+output "loadbalancer_address_name" {
+  description = "Address Name for Load Balancer"
+  value       = local.cluster__namespace__backstage__component__backend__lb_address__name
 }
 output "cluster_kubeconfig_update_command" {
   description = "Command to update kubeconfig, to use `terraform output -raw cluster_kubeconfig_update_command | sh`"
